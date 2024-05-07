fa5a4ecc3be0d9b790ecdaa12eb25f2b6978f542
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2024-05-07 16:25:03 -0700

    version bump - v0.2.2
    - fix extra / in paths

--- cmd/agedit/cli.go
+++ cmd/agedit/cli.go
@@ -22,7 +22,7 @@ import (
 const (
 	name          string = "agedit"
 	usage         string = "Edit age encrypted files with your $EDITOR"
-	version       string = "0.2.1"
+	version       string = "0.2.2"
 	help_template string = `NAME:
    {{.Name}} {{if .Version}}v{{.Version}}{{end}} - {{.Usage}}
 
