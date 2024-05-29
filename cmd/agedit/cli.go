a4f2f79c81df045ed067d89aa6f4e3ab706007e0
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2024-05-29 13:25:22 -0700

    version bump - v0.3.1
    - make it actually compile

--- cmd/agedit/cli.go
+++ cmd/agedit/cli.go
@@ -23,7 +23,7 @@ import (
 const (
 	name          string = "agedit"
 	usage         string = "Edit age encrypted files with your $EDITOR"
-	version       string = "0.3.0"
+	version       string = "0.3.1"
 	help_template string = `NAME:
    {{.Name}} {{if .Version}}v{{.Version}}{{end}} - {{.Usage}}
 
