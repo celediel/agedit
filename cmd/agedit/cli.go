6c9ae6bccd691308aa20a8349792b3705356826e
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2024-05-29 13:22:55 -0700

    version bump - v0.3.0
    - remove identity package
    - strip down env package
      - use adrg/xdg in lieu of (poorly) reinventing the wheel

--- cmd/agedit/cli.go
+++ cmd/agedit/cli.go
@@ -23,7 +23,7 @@ import (
 const (
 	name          string = "agedit"
 	usage         string = "Edit age encrypted files with your $EDITOR"
-	version       string = "0.2.3"
+	version       string = "0.3.0"
 	help_template string = `NAME:
    {{.Name}} {{if .Version}}v{{.Version}}{{end}} - {{.Usage}}
 
